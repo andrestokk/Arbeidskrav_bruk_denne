94ebcd1dd53f7fdf84005c80ebd5e0bce0fc0b82
André Stokkeland <andrestokk@hotmail.no>
2019-10-04 13:28:57 +0200

    Maven Fix for final Hand in

--- src/test/java/no/kristiania/arbeidskrav/romanNumbersConverterTest.java
+++ src/test/java/no/kristiania/arbeidskrav/romanNumbersConverterTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//AI: Oppretter testklasse
+//AI: Opprettet testklasse
 public class romanNumbersConverterTest {
 
     @Test
